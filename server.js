3d2be2ff612d1ea3533df23dc62f43ecb19f9070
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-09-11 10:38:06 +0600

    Inserted data to backend

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const mysql = require("mysql");
 const express = require("express");
-const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+// Create an instance of Express
+const app = express();
+// Use body-parser middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Define your routes and API endpoints here
+app.use(cors());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -24,12 +33,44 @@ app.get("/", (req, res) => {
   const query = "SELECT * FROM users";
   db.query(query, (err, result) => {
     if (err) {
-      console.error('Database query error:', err);
+      console.error("Database query error:", err);
       return res.sendStatus(500);
     }
     res.send(result);
   });
 });
+
+// Import necessary modules and establish database connection as previously shown
+
+// Define a route for user registration
+app.post("/register", (req, res) => {
+  console.log("Received registration request:", req.body);
+
+  // Extract user details from the request body
+  const { email, password, type } = req.body;
+
+  // Check if email, password, and type are provided
+  if (!email || !password || !type) {
+    return res.status(400).json({ error: "All fields are required." });
+  }
+
+  // Insert user data into the "users" table
+  const addUserQuery = `
+      INSERT INTO users (email, password, type)
+      VALUES (?, ?, ?)
+  `;
+
+  db.query(addUserQuery, [email, password, type], (err, result) => {
+    if (err) {
+      console.error("Error registering user:", err);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    console.log("User registered successfully", result);
+    return res.status(200).json({ message: "User registered successfully" });
+  });
+});
+
+// Rest of your server setup and routes
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
